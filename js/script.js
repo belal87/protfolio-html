2fb6dfe83095e786dd352894a6f06e60a2d54471
belal <mdbellhossain521@gmail.ocm>
2024-07-04 11:40:02 +0100

    added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-const getName = () => {
-  return "Belal Hossain";
-};
+// wirteFile
+// appendFile
+// readFile
+// rename
+// unLink
+// exsists
 
-console.log(getName());
+const http = require("http");
+const port = 8081;
+const hostname = "127.0.0.1";
+
+const myServer = http.createServer((req, res) => {
+  res.end("Hi this is http server ddfdf");
+});
+
+myServer.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}`);
+});
